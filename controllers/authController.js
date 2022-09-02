e53341a8f18f6af148ed978a699858484f5988b2
Tea M <55459419+ControlAltTea@users.noreply.github.com>
2022-09-02 10:42:22 -0700

    please

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,10 +5,10 @@ const User = require('../models/UserModel')
  exports.getLogin = (req, res) => {
    if (req.user) {
      console.log(`req.user`, req.user);
-      return res.redirect('/ships')
+      return res.redirect('/login')
     }
-    res.render('ships', {
-      title: 'ships',
+    res.render('login', {
+      title: 'Login',
     })
   }
   
