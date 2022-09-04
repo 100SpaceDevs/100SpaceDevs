b35d370f25b510dd8ea835b599165b68308b1d8d
Omi <digicoder@protonmail.com>
2022-09-04 17:53:55 -0400

    added a change event listener to the form inputs

--- public/js/accForm.js
+++ public/js/accForm.js
@@ -1,1 +1,11 @@
-(() => {})();
+(() => {
+  const form = document.querySelector(".form-container");
+
+  form.addEventListener("change", function (event) {
+    const { name, value } = event.target;
+    if (name === event.target.labels[0].htmlFor) {
+      if (value.length !== 0) {
+      }
+    }
+  });
+})();
