10304e6528cf131439a2b5cdd69ca860e90c6a3c
Alex Salazar <helloworld3197@gmail.com>
2022-09-05 20:32:48 -0400

    Updated Carousel

--- public/js/shipProfile.js
+++ public/js/shipProfile.js
@@ -18,6 +18,13 @@ const launchPhoto = document.querySelector(".launchPhoto");
 //   },
 // };
 
+let falconv1_0IdImg = "5eb87ce3ffd86e000604b336";
+let falcon1_1IdImg = "5eb87ce4ffd86e000604b337";
+let falcon1_2IdImg = "";
+let falconHeavyIdImg = "5eb87d2dffd86e000604b376";
+let falconB5IdIdImg = "5eb87d28ffd86e000604b373";
+// let falconImagesIdImg = [falconv1_0, falcon1_1];
+
 async function fetcher(id) {
   let url = `https://api.spacexdata.com/v5/launches/${id}`;
   const response = await fetch(url, {
@@ -29,8 +36,12 @@ async function fetcher(id) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const falcon1Img = data[13].links.flickr.original[1];
-      const falcon9Img = data[44].links.flickr.original[1];
+      const falcon1Img = data[12].links.flickr.original[0];
+      const falcon9Img = data[13].links.flickr.original[1];
+      const falcon1_1 = "";
+      const falcon1_2 = "";
+      const falconB5 = data[73].links.flickr.original[1];
+      const falconHeavy = data[76].links.flickr.original[2];
       launchPhoto.src = falcon1Img;
     });
   // console.log(data);
